c7306278c7538ea2a9fb6333c358646f1946c3a6
rkmanjunath <rashmikmanjunath@gmail.com>
2017-10-29 23:15:14 -0700

    Adds docstrings to the python functions
    Adds content to README.md

---
+++ README.md
@@ -0,0 +1,22 @@
+# FAERS Data Mining #
+
+The objective is to identify adverse drug events (ADEs) in the CMS claims data 
+with the aid of information from the US Food and Drug Administration (FDA) 
+Adverse Event Reporting System (FAERS) database. This effort will evaluate the 
+utility of CMS claims data for identifying potential adverse drug events and 
+if successful will improve the timeliness of ADE detection. Some of the studies 
+such as detecting association between Statin Use and Cancer (Mai Fujimoto, 2015), 
+Retrospective Detection of Drug Safety Signals and Adverse Events in Electronic 
+General Practice Records (Andrew Tomlin, 2012), Drug safety surveillance using 
+de-identified EMR and claims data (PM, 2010) and Leveraging Food and Drug 
+Administration Adverse Event Reports for the Automated Monitoring of Electronic 
+Health Records in a Pediatric Hospital (Tang H, 2017) present several approaches 
+that can be used.
+
+## Data Used ##
+* CMS 2008-2010 Data Entrepreneurs’ Synthetic Public Use Files (DE-SynPUF) for 
+  Beneficiary Summary, Inpatient Claims and Prescription Drug events.  
+* FAERS quarterly JSON files (drug-event) also called LAERS for the years 2008 – 2010.
+
+
+
--- src/faers_extract.py
+++ src/faers_extract.py
@@ -4,18 +4,16 @@ import os
 from pymongo import MongoClient
 
 
-def get_faers_data(text=" ", api_key="xBxthPuqSle59AwWI3GZZeaFOYZXrg2xxVh4BLBn"):
+def get_faers_data():
+    """Uses Python rest api to retrieve documents from the FAERS drug event end point
+     and prints out the documents. The function can be used during the initial analysis to understand the
+     document structure
+    :return: None
     """
-
-    :param test:
-    :param params:
-    :return:
-    """
-    params = {
-        'apikey': api_key,
-        'text': text,
-        'format': 'json'
-    }
+    # params = {
+    #     'apikey': api_key,
+    #     'format': 'json'
+    # }
 
     # URL = "https://api.fda.gov/drug/event.json?limit=1"
     URL = 'https://api.fda.gov/drug/event.json?search=receivedate:[20080101+TO+20080201]&count=receivedate'
@@ -25,58 +23,66 @@ def get_faers_data(text=" ", api_key="xBxthPuqSle59AwWI3GZZeaFOYZXrg2xxVh4BLBn")
 
     for result in data.get('results', []):
         print json.dumps(result, indent=2)
-    pass
+    return
 
 
 def read_json(json_file):
-    """
-
-    :param json_file:
-    :return:
+    """Read FAERS ADE json files, creates a mongodb instance "faersdb" and inserts the ADE reports into
+    ade_reports collection.
+    :param json_file: string/file path. Drug event files in json format (downloaded from FAERS drug event endpoint.
+    :return: None
     """
     client = MongoClient()
     client = MongoClient('localhost', 27017)
     db = client.faersdb
     with open(json_file, 'r') as infile:
         data = json.load(infile)
-    print data.keys()
     if "results" in data.keys():
         db.ade_reports.insert(data["results"])
     return
 
 
-def filter_data():
-    """
-
-    :param db:
-    :param collection:
-    :return:
+def filter_data(db, col_name):
+    """Filters Adverse event reports on the following criteria, and inserts the resulting reports into another
+    collection nlp_ade_reports.
+    * The report contained Drug dosage text
+    * Category of individual who submitted the report = health care professional(codes 1,2,3)
+    * Drug Characterization = 1 (Suspect - the drug was considered by the reporter to be the cause of ADE)
+    * Contains patient information and open fda fields
+    :param db: string/name of the database (faersdb)
+    :param col_name: string/name of the collection(ade_reports)
+    :return: None
     """
     client = MongoClient('localhost', 27017)
-    db = client.faersdb
-
-    cur = db.ade_reports.find(
-        {"patient.drug.patientonsetage": {"$exists": True},
-         "patient.drug.patientonsetageunit": {"$exists": True},
-         "patient.patientsex": {"$exists": True},
-         "patient.drug.openfda": {"$exists": True},
-         "primarysource.qualification": {"$in": ["1", "2", "3"]},
-         "patient.drug.drugcharacterization": "1",
-         "patient.drug.drugdosagetext": {"$exists": True},
-         })
+    db = client.db
+    collection = col_name
+    cur = db.collection.find({"$and":
+        [
+            {"primarysource.qualification": {"$in": ["1", "2", "3"]}},
+            {"patient.patientonsetage": {"$exists": True}},
+            {"patient.patientonsetageunit": {"$exists": True}},
+            {"patient.patientsex": {"$exists": True}},
+            {"patient.drug" : {"$elemMatch": {"openfda": {"$exists": True},
+                                                                "drugcharacterization": "1",
+                                                                "drugdosagetext": {"$exists": True}}}}
+        ]
+    })
     for elem in cur:
         db.nlp_ade_reports.insert(elem)
     return
 
 
-def extract_data():
-    """
-
-    :return:
+def extract_data(db, col):
+    """ Extarcts relevant fields from collection nlp_ade_reports and inserts them into ade_fields collection.
+    Different collections are used to store the data at different stages to facilitate iterative analysis.
+    :param db: string/name of the database (faersdb)
+    :param col: string/name of the collection(ade_reports)
+    :return: None
     """
     client = MongoClient('localhost', 27017)
-    db = client.faersdb
-    cur = db.nlp_ade_reports.find(
+    db = client.db
+    collection = col
+    cur = db.collection.find(
         {}, {"patient.patientonsetage": True,
              "patient.patientonsetageunit": True,
              "patient.patientsex": True,
@@ -90,10 +96,10 @@ def extract_data():
 
 
 def get_files(dir_path):
-    """
+    """Gives a list of all the files given a directory.
 
-    :param path:
-    :return:
+    :param dir_path: string/ directory path
+    :return:list containing all the file names in the directory
     """
     file_lst = []
     for name in os.listdir(dir_path):
@@ -102,10 +108,12 @@ def get_files(dir_path):
 
 
 if __name__ == "__main__":
-    # get_faers_data()
-    # dir = r"C:\Users\Gowri-Nidhi\IdeaProjects\rkmanjunath-final-project\Drugevents"
+    # get_faers_data(key)
+     dir = r"C:\Users\Gowri-Nidhi\IdeaProjects\rkmanjunath-final-project\Drugevents"
     # files = get_files(dir)
     # for file in files:
     #     read_json(file)
-    filter_data()
+    # filter_data()
     #extract_data()
+
+
